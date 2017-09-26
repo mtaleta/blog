a3d63693d9e6a3592597e1d9528036a36d3b456e
mtal <ex051240@gmail.com>
2017-09-26 20:30:24 +0800

    over

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import Promise from 'redux-promise';
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/post_show';
+
 
 const createStoreWithMiddleware = applyMiddleware(Promise)(createStore);
 
@@ -16,7 +18,8 @@ ReactDOM.render(
     <BrowserRouter>
       <div>
         <Switch>
-          <Route path="/posts/new" component={PostsNew}/>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/posts/:id" component={PostsShow} />
           <Route path="/" component={PostsIndex} />
         </Switch>
       </div>
